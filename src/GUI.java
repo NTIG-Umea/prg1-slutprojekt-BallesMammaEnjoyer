d5679cb39c80b29b231740295c8f53c5e179ab4c
BallesMammaEnjoyer <milletedebring.thysell@elev.ga.ntig.se>
2023-05-02 15:10:17 +0200

    Försökt med GUI

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI {
+    private JPanel panel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
